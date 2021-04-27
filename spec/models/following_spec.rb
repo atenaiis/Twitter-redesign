0ccab591b9e17745f9f8b98a70afb41f7df85c11
atenaiis <attennaisc@gmail.com>
2021-04-27 13:19:15 -0500

    add following test

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -1,12 +1,25 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
 RSpec.describe Following, type: :model do
-  let(:user) { User.create(username: 'name', fullname: 'name lastname') }
-
-  context '#Associations' do
-    it 'Following belongs to user' do
-      association = Following.reflect_on_association(:user)
-      expect(association.macro).to eq(:belongs_to)
+  before :each do
+    @user1 = User.new(full_name: 'User One', username: 'user1')
+    @user2 = User.new(full_name: 'User Two', username: 'user2')
     end
-  end
+  
+    it 'creates a invalid following' do
+      following = Following.new(follower_id: '', followed_id: '')
+      expect(following).to_not be_valid
+    end
+    context 'Association tests' do
+      it 'belongs to follower' do
+        assc = Following.reflect_on_association(:follower)
+        expect(assc.macro).to eq :belongs_to
+      end
+  
+      it 'belongs to followed' do
+        assc = Following.reflect_on_association(:followed)
+        expect(assc.macro).to eq :belongs_to
+      end
 end
+end
+
