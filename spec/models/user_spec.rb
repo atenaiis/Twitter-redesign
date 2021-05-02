8930258b1af503df24bc4e15d055ff5b56854382
atenaiis <attennaisc@gmail.com>
2021-05-02 18:07:54 -0500

    Add spec and tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'Validation tests' do
+    it 'validates attributes - invalid' do
+      user = User.new(full_name: '', username: '', photo: '', cover_image: '')
+      expect(user.valid?).to be(false)
+    end
+   end
+
+  context 'Association tests' do
+    it 'has many opinions' do
+      assc = User.reflect_on_association(:opinions)
+      expect(assc.macro).to eq :has_many
+    end
+
+    it 'has many followings' do
+      assc = User.reflect_on_association(:followings)
+      expect(assc.macro).to eq :has_many
+    end
+
+    it 'has many followers' do
+      assc = User.reflect_on_association(:followers)
+      expect(assc.macro).to eq :has_many
+    end
+  end
+end
